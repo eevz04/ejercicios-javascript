1d061311e579be5e4ea040d357bf234b6b643236
eevz04 <eeliasvaldez@hotmail.com>
2026-03-03 16:56:05 -0300

    Implemento función que retorna índice mínimo y máximo en array de páginas de libros

--- ejercicios.js
+++ ejercicios.js
@@ -171,6 +171,21 @@ function findJavaScript(matrix) {
     return [-1, -1]            
 }
 
+// Ejercicio 8
+// En una biblioteca queremos saber qué libro es el que menos páginas tiene y el que más páginas. Por suerte, no hay dos libros con el mismo número de páginas.
+// Necesitamos que la función reciba un array de números, sin ordenar, y que devuelva un array de dos posiciones con el índice del libro con menos páginas y el índice del libro con más páginas.
+
+function minAndMaxWord(words) {
+    let minIndex = 0
+    let maxIndex = 0
+
+    
+
+
+
+}
+
+
 
 
 
